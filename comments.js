999fd1a86ff1780589633141eef1f6a76a69c0e0
Bernd Sonderkoetter <Bernd.Sonderkoetter@eviden.com>
2025-02-11 19:27:22 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// Load the comments
+// Add the comments
+// Save the comments
+// Send the comments
+// End
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
