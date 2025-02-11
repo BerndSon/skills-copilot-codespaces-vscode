4767d9c11f58c0661788dd72ab2d6c557a46e365
Bernd Sonderkoetter <Bernd.Sonderkoetter@eviden.com>
2025-02-11 19:22:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'Express',
+    'MongoDB',
+    'Python',
+    'Django',
+    'Flask',
+  ];
+
+  return skills;
+}
